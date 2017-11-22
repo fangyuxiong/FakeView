821b5f4353db7af5bbfbfb6d5fbd1b515191733a
XiongFangyu <xiong.fangyu@immomo.com>
2017-11-22 11:56:51 +0800

    forget

--- library/src/main/java/xfy/fakeview/library/translator/event/ViewLike.java
+++ library/src/main/java/xfy/fakeview/library/translator/event/ViewLike.java
@@ -22,4 +22,24 @@ class ViewLike implements IView {
     public int getId() {
         return id;
     }
+
+    @Override
+    public Object getTag() {
+        return null;
+    }
+
+    @Override
+    public void setTag(Object tag) {
+
+    }
+
+    @Override
+    public Object getTag(int key) {
+        return null;
+    }
+
+    @Override
+    public void setTag(int key, Object tag) {
+
+    }
 }
